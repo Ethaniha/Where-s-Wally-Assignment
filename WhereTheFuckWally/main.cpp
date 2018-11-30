f3ec8e2e6351fb61887d0bc92f6346c20a084aed
Ethan Richardson <ethan@ethans-mac-pro.local>
2018-11-30 17:56:32 +0000

    Initial Commit

---
+++ WhereTheFuckWally/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WhereTheFuckWally
+//
+//  Created by Ethan Richardson on 30/11/2018.
+//  Copyright © 2018 Ethan Richardson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
